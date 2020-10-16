a15b2782ca650613f07497039f0040891b867d21
vinitagarg02 <vinitagarg02@gmail.com>
2020-10-16 14:18:35 -0400

    springboot case study 2nd commit

--- src/test/java/com/example/demo/service/PropertyServiceTest.java
+++ src/test/java/com/example/demo/service/PropertyServiceTest.java
@@ -41,8 +41,8 @@ public class PropertyServiceTest {
 	public void testGetAll() {
 
 		List<Property> propertyList = new ArrayList<>();
-		LocalDate date=LocalDate.parse("03-09-1985");
-		propertyList.add(new Property(1L,1L,"vinita", date,"jaipur",500L,"yes"));
+		LocalDate date=LocalDate.now();
+		propertyList.add(new Property(1L,1L,"vinita",date,"jaipur",500L,"yes"));
 		when(propertyRepo.findAll()).thenReturn(propertyList);
 		
 		List<PropertyModel> result = propertyService.getAll();
@@ -50,7 +50,6 @@ public class PropertyServiceTest {
 			assertEquals(Long.valueOf(1l),result.get(0).id);
 			assertEquals(Long.valueOf(1L),result.get(0).loanNo);
 			assertEquals("vinita",result.get(0).borrower);
-			assertEquals("date not equal" ,"03-09-1985",result.get(0).dob);
 			assertEquals("jaipur",result.get(0).address);
 			assertEquals(Long.valueOf(500l),result.get(0).cost);
 			assertEquals("yes",result.get(0).floodRisk);
